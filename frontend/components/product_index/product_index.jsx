f343f3f55b7e88b9e6bd42ba81a55f156f83472b
Nick Pietrow <njpietrow@gmail.com>
2022-03-21 12:22:12 -0700

    added product index path palceholder and classnames to style containers for product index

--- frontend/components/product_index/product_index.jsx
+++ frontend/components/product_index/product_index.jsx
@@ -10,6 +10,7 @@ class ProductIndex extends React.Component{
     const {products} = this.props
     return(
       <div className="product-index-container">
+        <h3 className="product-index-path">Home / Collections / All Coffee</h3>
         <ul className="product-index-content-container">
           {products.map( product => (
               <ProductIndexItem 
