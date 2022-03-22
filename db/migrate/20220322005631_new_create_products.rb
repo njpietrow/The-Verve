c4a71747fcb294fd47e83b97693c8da4fed09ed5
Nick Pietrow <njpietrow@gmail.com>
2022-03-21 17:57:25 -0700

    assign new id for product table migration

--- db/migrate/20220322005631_new_create_products.rb
+++ db/migrate/20220322005631_new_create_products.rb
@@ -1,4 +1,4 @@
-class CreateProducts < ActiveRecord::Migration[5.2]
+class NewCreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.string :product_name, null: false
