aa5ed3a2b67165518801c977decd5a4bcfdbf476
Nick Pietrow <njpietrow@gmail.com>
2022-03-21 23:16:44 -0700

    refactored products reducer

--- frontend/reducers/products_reducer.js
+++ frontend/reducers/products_reducer.js
@@ -2,12 +2,13 @@ import { RECEIVE_PRODUCTS, RECEIVE_PRODUCT } from "../actions/product_actions"
 
 const productReducer = (state={}, action) => {
   Object.freeze(state);
-  let nextState = Object.assign({}, state);
+  let nextState;
   switch (action.type) {
     case RECEIVE_PRODUCTS:
       nextState =  Object.assign({}, action.products);
       return nextState;
     case RECEIVE_PRODUCT:
+      nextState = Object.assign({}, state);
       nextState[action.product.id] = action.product
       return nextState;
     default:
