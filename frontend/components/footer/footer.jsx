9887f33945ff03d1037c552cdc6b8817de9ff961
Nick Pietrow <njpietrow@gmail.com>
2022-03-31 11:27:00 -0700

    updated footer and flexbox for header container

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -23,10 +23,17 @@ const Footer = () => (
           <ul></ul>
         </div>
         <div className="two">
-          <p>WE BELIEVE THE COFFEE EXPERIENCE IS OUR RESPONSIBILITY FROM SEED TO CUP. COFFEE IS OUR CRAFT, OUR RITUAL, OUR PASSION. IT DRIVES US AND INSPIRES US. WITH THIS SIMPLE TRUTH AND RESPONSIBILITY WE ARE BRIDGING THE GAP FROM FARMLEVEL TO STREETLEVEL.
+          <p>
+            WE BELIEVE THE COFFEE EXPERIENCE IS OUR RESPONSIBILITY FROM SEED TO CUP. COFFEE IS OUR CRAFT, OUR RITUAL, OUR PASSION. IT DRIVES US AND INSPIRES US. WITH THIS SIMPLE TRUTH AND RESPONSIBILITY WE ARE BRIDGING THE GAP FROM FARMLEVEL TO STREETLEVEL.
             <br />
             <br />
-            WE ARE VERVE. MADE IN SANTA CRUZ.</p>
+            WE ARE VERVE. MADE IN SANTA CRUZ.
+          </p>
+          <p> 
+            <a href="https://github.com/njpietrow/The-Verve"><i className="fa-brands fa-github"></i></a>
+            <a href="https://angel.co/u/nick-pietrow"><i className="fa-brands fa-angellist"></i></a>
+            <a href="https://twitter.com/njpietrow"><i className="fa-brands fa-twitter"></i></a>
+          </p>
         </div>
         <div className="three">
           <div>
@@ -35,14 +42,21 @@ const Footer = () => (
             </p>
           </div>
         </div>
-
       </div>
-      <div className="margin-spacer">
-
-      </div>
+      <div className="margin-spacer"></div>
       <div className="footer-address-blocks">
-
-      </div>
+        <div className="block align-left">
+          © Nick Pietrow 2022 <br />
+          ALL RIGHTS RESERVED
+        </div>
+        <div className="block">
+          The Verve <br />
+          Los Angeles, CA
+        </div>
+        <div className="block align-right">
+          PROUDLY CRAFTED IN LOS ANGELES, CA
+        </div>
+      </div>  
     </div>
   </footer>
 );
