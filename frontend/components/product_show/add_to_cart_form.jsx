9ba933c2dd22d4125938ea9520d494da005c8d9f
Nick Pietrow <njpietrow@gmail.com>
2022-03-30 18:10:46 -0700

    updated quantity add form on product show page

--- frontend/components/product_show/add_to_cart_form.jsx
+++ frontend/components/product_show/add_to_cart_form.jsx
@@ -88,7 +88,7 @@ class AddToCartForm extends React.Component{
         <div className="quantity-add-container">
           <a className="quantity-add minus" onClick={() => this.updateQuantity(-1)}></a>
           <input type="text"
-            className="input-box" 
+            className="input-box quantity-product" 
             value={this.state.quantity}  
             pattern="(500|([1-4][0-9][0-9])|([1-9][0-9])|[1-9])" 
             title="Quantity should contain only positive numerical values and be less than 500" 
